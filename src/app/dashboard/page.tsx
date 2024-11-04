569533d46bcbd3dc46ee73fe451e0ca14545cd30
ecoscom <evandrodacosta@gmail.com>
2024-11-04 18:12:45 -0300

    Add dashboad page (incomplete)

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Dashboard(){
+    return (
+        <div className="">
+            <Head>
+                <title>Meu Painel</title>
+            </Head>
+            <h1>Página Painel</h1>
+
+        </div>
+    )
+}
